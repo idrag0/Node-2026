6808b1620ecd9610260ff302f7cebb7b03a68e86
Gufran Ahmad <33379735+Gstarinfo@users.noreply.github.com>
2026-02-24 22:28:07 +0530

    server

--- Lec1-10/server_L7.js
+++ Lec1-10/server_L7.js
@@ -6,3 +6,11 @@ http.createServer((req  , res) =>{
     res.write("<h1>hello world</h1>");
     res.end("<p>this is my first server</p>");
 }).listen(3800);
+
+
+
+
+http.createServer((req  , res) =>{
+    res.write("<h1>hello other world</h1>");
+    res.end("<p>this is my first server</p>");
+}).listen(5800);
