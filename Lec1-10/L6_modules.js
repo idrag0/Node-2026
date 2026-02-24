7d3eab1630dff68eb8ab2c15c4eac554553c2613
Gufran Ahmad <33379735+Gstarinfo@users.noreply.github.com>
2026-02-24 12:31:52 +0530

    node server

---
+++ Lec1-10/L6_modules.js
@@ -0,0 +1,17 @@
+
+
+// const fs = require('fs')
+
+// fs.writeFileSync("./Lec1-10/dummy.txt", "This is dummy text");
+
+// const os = require('os');
+// console.log(os.platform());
+// console.log(os.arch());
+// console.log(os.hostname());
+
+
+
+console.log("objects in nodejs are called modules");
+console.log(process.cwd());
+console.log(process.pid());
+
---
+++ Lec1-10/server_L7.js
@@ -0,0 +1,8 @@
+
+
+const http = require('http');
+
+http.createServer((req  , res) =>{
+    res.write("<h1>hello world</h1>");
+    res.end("<p>this is my first server</p>");
+}).listen(3800);
