3236c82ccabfbc95dd3d6f77cb29de26416d1402
Gufran Ahmad <33379735+Gstarinfo@users.noreply.github.com>
2026-03-03 11:56:27 +0530

    Lec 17 in form 15.js create file and write file

--- lec11-20/form_L15.js
+++ lec11-20/form_L15.js
@@ -4,6 +4,8 @@ const http = require("http");
 
 const fs = require("fs");
 
+const querystring = require("querystring");
+
 // http.createServer((req, res) => {
 //     res.writeHead(201, {"Content-Type" : "text/html"})
 //     if(req.url == "/"){
@@ -32,14 +34,14 @@ http.createServer((req, res) => {
 
         if (err) {
             res.writeHead(500, { "Content-Type": "text/html" });
-            res.writable("internal server error");
+            res.write("internal server error");
             res.end();
             return;
         }
         if (req.url == "/") {
             res.writeHead(201, { "Content-Type": "text/html" });
             res.write(data);
-        } else if (req.url == "/submit") {
+        } else if (req.url == "/submit") {    // Lecture 16
 
             let databody = [];
 
@@ -49,7 +51,19 @@ http.createServer((req, res) => {
 
             req.on('end', ()=>{
                 let rawData = Buffer.concat(databody).toString();
-                console.log(rawData);
+                let readable =  querystring.parse(rawData);
+                let str  = "name : " + readable.name + " Email : " + readable.email + " Phone : " + readable.phone + " Country : " + readable.country + " Message : " + readable.message;
+                console.log(str);
+
+                // fs.writeFileSync(readable.name + ".txt", str);
+
+                fs.writeFile("./data.txt", str + "\n", {flag: "a"}, (err) => {
+                    if(err){
+                        console.log("error writing file");
+                    }
+                })
+
+                console.log("data addedd to file");
             });
             
             res.write("<p> data submited guys</p>");
