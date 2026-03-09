b325ca7c5dea95c03f3aac577da9cfbb97e7f792
Gufran Ahmad <33379735+Gstarinfo@users.noreply.github.com>
2026-03-09 12:15:50 +0530

    lec 28 loading html page

---
+++ Lec28/index_L28.js
@@ -0,0 +1,26 @@
+
+
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+app.get('/', (req, res) =>{
+    const abspath = path.resolve('./view/home.html');
+    res.sendFile(abspath);
+});
+
+
+app.get('/login', (req,res)=>{
+    const abspath = path.resolve('./view/login.html');
+    res.sendFile(abspath);
+})
+
+app.get('/about', (req,res)=>{
+    const abspath = path.resolve('./view/about.html');  
+    res.sendFile(abspath);      
+});
+
+app.listen(8600, () => {
+    console.log('Server is running on port 8600');
+});
