b7d8f16c3b52a68db228167c8995b403d85de02d
Gufran Ahmad <33379735+Gstarinfo@users.noreply.github.com>
2026-03-09 13:18:07 +0530

    lec 28

--- Lec28/index_L28.js
+++ Lec28/index_L28.js
@@ -21,6 +21,12 @@ app.get('/about', (req,res)=>{
     res.sendFile(abspath);      
 });
 
+app.use((req, res) => {
+    
+    const abspaath = path.resolve('./view/404.html');
+    res.status(404).sendFile(abspaath);
+});
+
 app.listen(8600, () => {
     console.log('Server is running on port 8600');
 });
